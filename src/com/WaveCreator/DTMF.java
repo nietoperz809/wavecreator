342dcf658b3b08a27954bcb65764131294bdc58c
Administrator <Administrator@toschi>
2013-09-07 19:56:47 +0200

    sicherheitskopie

--- src/com/WaveCreator/DTMF.java
+++ src/com/WaveCreator/DTMF.java
@@ -1,5 +1,7 @@
 package com.WaveCreator;
 
+import java.util.HashMap;
+
 /**
  * Creates DTMF samples
  */
@@ -17,11 +19,8 @@ public class DTMF
     /**
      * Map of all samples
      */
-    private final Wave16[] wavemap;
-    /**
-     * Map of characters that map to Wave16 objects
-     */
-    private final char[] charmap;
+    private final HashMap<Character, Wave16> wavemap = new HashMap<Character, Wave16>();
+    
 
     /**
      * Main Constructor: Builds all DTMF samples
@@ -53,8 +52,23 @@ public class DTMF
         Wave16 ssharp = wv.functionsGenerators.curveSine(samplingRate, new double[]{1477, 941}, samples);
         Wave16 SD = wv.functionsGenerators.curveSine(samplingRate, new double[]{1633, 941}, samples);
 
-        wavemap = new Wave16[]{s1, s2, s3, SA, s4, s5, s6, SB, s7, s8, s9, SC, sstar, s0, ssharp, SD};
-        charmap = new char[]{'1', '2', '3', 'A', '4', '5', '6', 'B', '7', '8', '9', 'C', '*', '0', '#', 'D'};
+        wavemap.put('1', s1);
+        wavemap.put('2', s2);
+        wavemap.put('3', s3);
+        wavemap.put('A', SA);
+        wavemap.put('4', s4);
+        wavemap.put('5', s5);
+        wavemap.put('6', s6);
+        wavemap.put('B', SB);
+        wavemap.put('7', s7);
+        wavemap.put('8', s8);
+        wavemap.put('9', s9);
+        wavemap.put('C', SC);
+        wavemap.put('*', sstar);
+        wavemap.put('0', s0);
+        wavemap.put('#', ssharp);
+        wavemap.put('D', SD);
+        
         delay = delaysamples;
     }
 
@@ -66,23 +80,6 @@ public class DTMF
     public DTMF(int rate, int samples)
     {
         this(rate, samples, 0);
-    }
-
-    /**
-     * Gets DTMF object from character
-     * @param c Character that should be converted
-     * @return The sampling object or <b>null</b> if not found
-     */
-    private Wave16 fromChar(char c)
-    {
-        for (int s = 0; s < charmap.length; s++)
-        {
-            if (charmap[s] == c)
-            {
-                return wavemap[s];
-            }
-        }
-        return null;
     }
 
     /**
@@ -98,7 +95,7 @@ public class DTMF
             inWave = new Wave16[in.length()];
             for (int s = 0; s < in.length(); s++)
             {
-                inWave[s] = fromChar(in.charAt(s));
+                inWave[s] = wavemap.get(in.charAt(s));
             }
         }
         else
@@ -107,7 +104,7 @@ public class DTMF
             Wave16 delWave = new Wave16(delay, samplingRate);
             for (int s = 0; s < in.length(); s++)
             {
-                inWave[s * 2] = fromChar(in.charAt(s));
+                inWave[s * 2] = wavemap.get(in.charAt(s));
                 inWave[s * 2 + 1] = delWave;
             }
         }
